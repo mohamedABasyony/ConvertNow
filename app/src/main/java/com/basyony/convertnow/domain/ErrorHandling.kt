57b79416ba5d89c8f1d532e30f72391c394b6c17
mohamedabasyony <mohamed.adel.basyony@gmail.com>
2019-11-18 22:53:25 +0200

    Add error handler

--- app/src/main/java/com/basyony/convertnow/domain/ErrorHandling.kt
+++ app/src/main/java/com/basyony/convertnow/domain/ErrorHandling.kt
@@ -1,8 +1,18 @@
 package com.basyony.convertnow.domain
 
-import com.basyony.convertnow.entities.AppError
+import android.accounts.NetworkErrorException
+import com.basyony.convertnow.entities.*
+import java.net.UnknownHostException
+import java.net.UnknownServiceException
+import java.util.concurrent.TimeoutException
 
 fun errorMapper(throwable: Throwable): AppError {
     throwable.printStackTrace()
-    TODO("Not Implemented")
+    return when (throwable.cause) {
+        is NetworkErrorException -> NoInternet
+        is UnknownHostException -> NoInternet
+        is UnknownServiceException -> ServerDown
+        is TimeoutException -> TimeOut
+        else -> Business
+    }
 }
--- app/src/main/java/com/basyony/convertnow/presentation/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/basyony/convertnow/presentation/dashboard/DashboardViewModel.kt
@@ -1,6 +1,7 @@
 package com.basyony.convertnow.presentation.dashboard
 
 import androidx.lifecycle.MutableLiveData
+import com.basyony.convertnow.domain.errorMapper
 import com.basyony.convertnow.domain.gateways.RatesAPI
 import com.basyony.convertnow.domain.toDashboardUiModel
 import com.basyony.convertnow.entities.*
@@ -22,7 +23,7 @@ class DashboardViewModel (
             .compose { composer(it) }
             .subscribe(
                 { screenData.postValue(Success(it.toDashboardUiModel())) },
-                { it.printStackTrace() }
+                { screenData.postValue(ScreenError(errorMapper(it))) }
             )
             .disposeBy(disposables)
     }
